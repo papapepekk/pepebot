08154f9e4f6ec3b91f2de60b74570f21c9acfffb
papapepekk <77702436+papapepekk@users.noreply.github.com>
2021-02-06 14:52:03 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,113 @@
+
+const help = (prefix) => { 
+	return `                 
+<══════════════════════>
+            CREITIM - BOT <══════════════════════>
+
+  __█████████         
+  __█▄█████▄█   Author:ELɢᴜsᴛᴀ
+  __█▼▼▼▼▼█ 
+  _██CREITIM██▌ 
+  __█▲▲▲▲▲█ 
+  __█████████ 
+  ____██_____██
+         
+      ☠️ C R E I T I M  B O T☠️
+    
+╔══════════════════════
+║       LISTA DE COMANDOS✓
+╠══════════════════════
+
+ ┣━━✓ *Produçao* ✓━━
+ ┣⊱ *${prefix}ocr*
+ ┣⊱ *${prefix}sticker*
+ ┣⊱ *${prefix}tts*
+ ┣⊱ *${prefix}toimg*
+ ┣⊱ *${prefix}phlogo*
+ ┣⊱ *${prefix}quotemaker*
+ ┣⊱ *${prefix}ninjalogo*
+ ┣⊱ *${prefix}glitch*
+ ┣⊱ *${prefix}text3d*
+
+ ┣━━✓ *DIVERSÃO* ✓━━━━
+ ┣⊱ *${prefix}meme*
+ ┣⊱ *${prefix}memeindo*
+ ┣⊱ *${prefix}hilih*
+ ┣⊱ *${prefix}fitnah*
+ ┣⊱ *${prefix}persengay*(nivel gay)
+
+ ┣━━✓ *Anime* ✓━━
+ ┣⊱ *${prefix}loli*
+ ┣⊱ *${prefix}nsfwloli*
+ ┣⊱ *${prefix}anime*
+ ┣⊱ *${prefix}randomanime*
+ ┣⊱ *${prefix}randomhentai*
+ ┣⊱ *${prefix}nsfwloli*
+ ┣⊱ *${prefix}nsfwblowjob*
+ ┣⊱ *${prefix}nsfwneko*
+ ┣⊱ *${prefix}nsfwtrap*
+
+ ┣━━✓ *Media* ✓━━
+ ┣⊱ *${prefix}tiktokstalk*
+ ┣⊱ *${prefix}ytmp4*
+
+ ┣━━✓ *Outros* ✓━━
+ ┣⊱ *${prefix}shorturl*(url curto)
+ ┣⊱ *${prefix}fototiktok*
+ ┣⊱ *${prefix}map*
+ ┣⊱ *${prefix}infogempa (informações sobre terremoto)*
+ ┣⊱ *${prefix}quotes*(citações)
+ ┣⊱ *${prefix}cerpen*(historia curta)
+ ┣⊱ *${prefix}pokemon*
+ ┣⊱ *${prefix}chord *(acorde)
+ ┣⊱ *${prefix}blocklist*
+ ┣⊱ *${prefix}hidetag*
+ ┣⊱ *${prefix}wolflogo*
+ ┣⊱ *${prefix}lionlogo*
+ ┣⊱ *${prefix}ssweb*
+ ┣⊱ *${prefix}anjing*(cachorro)
+ ┣⊱ *${prefix}indohot*
+
+ ┣━━✓ *Grupo* ✓━━
+ ┣⊱ *${prefix}linkgc*
+ ┣⊱ *${prefix}tagall*
+ ┣⊱ *${prefix}add* [+62xx]
+ ┣⊱ *${prefix}group* [buka/tutup]
+ ┣⊱ *${prefix}demote* [tag]
+ ┣⊱ *${prefix}promote* [tag]
+ ┣⊱ *${prefix}kick*
+ ┣⊱ *${prefix}listadmin*
+ ┣⊱ *${prefix}simih* [1/0]
+ ┣⊱ *${prefix}nsfw* [1/0]
+ ┣⊱ *${prefix}welcome* [1/0]
+
+ ┣━━✓ *proprietario do bot* ✓━
+ ┣⊱ *${prefix}clearall*
+ ┣⊱ *${prefix}block*
+ ┣⊱ *${prefix}unblock*
+ ┣⊱ *${prefix}bc*
+ ┣⊱ *${prefix}leave*
+ ┣⊱ *${prefix}setpp*
+ ┣⊱ *${prefix}setprefix*
+ ┣━━━━━━━━━━━━━━
+
+ ┏━━✓ *INFORMAÇOES DO BOT* ✓━━┓
+ ┣⊱ *${prefix}owner*
+ ┣⊱ *${prefix}info*
+ ┣⊱ *${prefix}botstat*
+ ┣⊱ *${prefix}ping
+se quiser falar com meu dono,chame ele neste numero:wa.me/+5561993845817 :)
+         
+            C R E I T I M  B O T
+          
+         
+     ┌─────────😎────────┐
+              sʜɪᴛs_ᴍᴀɴɢᴀ_ʀᴏsᴀ 
+     └─────────😎────────┘
+`
+}
+exports.help = help
+
+
+
+  
